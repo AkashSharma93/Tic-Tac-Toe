c8c028ad0ffa7e0bec63ed7f453a5b825f4d2bb1
Suigetsu <present.akash@gmail.com>
2013-12-28 18:18:33 +0530

    Added bounds to the frame.

--- Lobby.java
+++ Lobby.java
@@ -1,17 +1,37 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Lobby {
-	Player player;
+//	Player player;
 	JFrame frame;
 
-	public Lobby(Player p) {
-		player = p;
-	}
+//	public Lobby(Player p) {
+//		player = p;
+//	}
 
 	public void showLobby() {
 		frame = new JFrame("Lobby");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+		Bounds b = new Bounds();
+		frame.getContentPane().add(BorderLayout.CENTER, b);
+
 		frame.setVisible(true);
+	}
+
+	public class Bounds extends JPanel{
+		public void paintComponent(Graphics g) {
+			int width = frame.getWidth();
+			int height = frame.getHeight();
+
+			g.drawLine(width/4, 0, width/4, height);
+			g.drawLine(3*width/4, 0, 3*width/4, height);
+		}
+	}
+
+	public static void main(String[] args) {
+		Lobby l = new Lobby();
+		l.showLobby();
 	}
 }
