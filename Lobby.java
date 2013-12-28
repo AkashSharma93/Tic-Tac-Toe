4506b32bb14701b44171f45a2153afaf2b99e87d
Suigetsu <present.akash@gmail.com>
2013-12-28 17:15:00 +0530

    The Lobby where the players interact.

---
+++ Lobby.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Lobby {
+	Player player;
+	JFrame frame;
+
+	public void setPlayer(Player p) {
+		player = p;
+	}
+
+	public void showLobby() {
+		frame = new JFrame("Lobby");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		frame.setVisible(true);
+	}
+}
