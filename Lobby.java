0b21315cfeb2680da123013ca9b6ce3f4ed7f5e9
Suigetsu <present.akash@gmail.com>
2013-12-30 17:54:51 +0530

    Got the lines back. It's finally working along with the labels.

--- Lobby.java
+++ Lobby.java
@@ -4,6 +4,7 @@ import java.awt.*;
 public class Lobby {
 	Player player;
 	JFrame frame;
+	Bounds b;
 
 	public Lobby(Player p) {
 		player = p;
@@ -14,10 +15,10 @@ public class Lobby {
 		frame = new JFrame("Lobby");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		frame.setLayout(null);
 
-//		Bounds b = new Bounds();
-//		frame.add(b);
+		b = new Bounds();
+		b.setLayout(null);
+		frame.getContentPane().add(BorderLayout.CENTER, b);
 
 		displayStat();
 		
@@ -30,24 +31,24 @@ public class Lobby {
 
 		heading1 = new JLabel("Player details...");
 		heading1.setBounds(10, 10, 150, 20);
-		frame.add(heading1);
+		b.add(heading1);
 
 		heading2 = new JLabel("Player Statistics...");
 		heading2.setBounds(10, 60, 150, 50);
-		frame.add(heading2);
+		b.add(heading2);
 
-//		playerName = new JLabel(player.getName());
-//		playerName.setBounds(10, 20, 150, 30);
-//		frame.add(playerName);
+		playerName = new JLabel(player.getName());
+		playerName.setBounds(10, 20, 150, 30);
+		b.add(playerName);
 	}
 
-//	public class Bounds extends JPanel{
-//		public void paintComponent(Graphics g) {
-//			int width = frame.getWidth();
-//			int height = frame.getHeight();
+	public class Bounds extends JPanel{
+		public void paintComponent(Graphics g) {
+			int width = frame.getWidth();
+			int height = frame.getHeight();
 
-//			g.drawLine(width/4, 0, width/4, height);
-//			g.drawLine(3*width/4, 0, 3*width/4, height);
-//		}
-//	}
+			g.drawLine(width/4, 0, width/4, height);
+			g.drawLine(3*width/4, 0, 3*width/4, height);
+		}
+	}
 }
