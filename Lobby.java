e593c86ea37c53cdd4ebbd5ef6830786eedb3c95
Suigetsu <present.akash@gmail.com>
2013-12-30 19:52:25 +0530

    Added a button. Tried adding a text area to display the players but I don't know why but it just doesn't work with a scrollpane. So, yeah, no scrollpane right now... I think I pretty much broke this module. XD

--- Lobby.java
+++ Lobby.java
@@ -5,6 +5,9 @@ public class Lobby {
 	Player player;
 	JFrame frame;
 	Bounds b;
+	JButton newChallenge;
+	JTextArea playerList;
+	int width, height;
 
 	public Lobby(Player p) {
 		player = p;
@@ -16,13 +19,22 @@ public class Lobby {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
+		frame.setVisible(true);
+
 		b = new Bounds();
 		b.setLayout(null);
 		frame.getContentPane().add(BorderLayout.CENTER, b);
 
 		displayStat();
+		displayButtons();
+
+		try {
+			Thread.sleep(100);
+		} catch(Exception ex) {}
+
+		displayText();
 		
-		frame.setVisible(true);
+		frame.repaint();
 	}
 
 	public void displayStat() {
@@ -34,7 +46,7 @@ public class Lobby {
 		b.add(heading1);
 
 		heading2 = new JLabel("Player Statistics...");
-		heading2.setBounds(10, 60, 150, 50);
+		heading2.setBounds(10, 60, 150, 20);
 		b.add(heading2);
 
 		playerName = new JLabel(player.getName());
@@ -42,10 +54,35 @@ public class Lobby {
 		b.add(playerName);
 	}
 
+	public void displayButtons() {
+		newChallenge = new JButton("New challenge");
+		newChallenge.setBounds(10, 200, 150, 20); 
+		b.add(newChallenge);
+	}
+
+	public void displayText() {
+
+		System.out.println(height);
+		System.out.println(width);
+
+		playerList = new JTextArea(width, height);
+		playerList.setLineWrap(true);
+		playerList.setWrapStyleWord(true);
+		playerList.setEditable(false);
+
+//		JScrollPane scroller1 = new JScrollPane(playerList);
+//		scroller1.setBounds(100, 100, 400, 500);
+
+		playerList.setBounds(3*width/4, 0, width/4, height);
+		playerList.setText("Display players here...");
+
+		b.add(playerList);
+	}
+
 	public class Bounds extends JPanel{
 		public void paintComponent(Graphics g) {
-			int width = frame.getWidth();
-			int height = frame.getHeight();
+			width = frame.getWidth();
+			height = frame.getHeight();
 
 			g.drawLine(width/4, 0, width/4, height);
 			g.drawLine(3*width/4, 0, 3*width/4, height);
