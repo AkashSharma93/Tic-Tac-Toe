26fb77165b569f32957bf8c38ed79e9d80e02879
Suigetsu <present.akash@gmail.com>
2014-01-09 20:33:50 +0530

    Moving away from absolute positioning. Having a hard time with layout managers... But this should be enough to get started with this.

--- Lobby.java
+++ Lobby.java
@@ -4,17 +4,56 @@ import java.awt.*;
 public class Lobby {
 	Player player;
 	JFrame frame;
-	Bounds b;
-	JButton newChallenge;
-	JTextArea playerList;
-	int width, height;
+//	Bounds b;
+//	JButton newChallenge;
+//	JTextArea playerList;
+	JPanel mainPanel, playerPanel, centerPanel, playerListPanel;
+	JList playerList, battleRequestList;
+//	int width, height;
 
 	public Lobby(Player p) {
 		player = p;
 		showLobby();
 	}
-
+	
 	public void showLobby() {
+		frame = new JFrame("Lobby");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		
+		mainPanel = new JPanel();
+		
+		//Player details.
+		playerPanel = new JPanel();
+		
+		//Centre Area.
+		centerPanel = new JPanel();
+		battleRequestList = new JList();
+		JScrollPane scroller1 = new JScrollPane(battleRequestList);
+		scroller1.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scroller1.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		centerPanel.add(scroller1);
+		
+		//Player List
+		playerListPanel = new JPanel();
+		playerList = new JList();
+		JScrollPane scroller2 = new JScrollPane(playerList);
+		scroller2.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scroller2.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		playerListPanel.add(scroller2);
+		
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
+		mainPanel.add(playerPanel);
+		mainPanel.add(centerPanel);
+		mainPanel.add(playerListPanel);
+		
+		frame.getContentPane().add(mainPanel, BorderLayout.CENTER);
+		
+		frame.setVisible(true);
+	}
+}
+	
+/*	public void showLobby() {
 		frame = new JFrame("Lobby");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
@@ -89,3 +128,4 @@ public class Lobby {
 		}
 	}
 }
+*/
